c7b681825ccf281bf0bff83f48fa7dd4d0b3b936
kareemtaha-coder <engkareemoo20@gmail.com>
2026-05-31 06:30:13 +0300

    Add Global FixedWindowRateLimiter

--- RateLimitAppDemo/Controllers/WeatherForecastController.cs
+++ RateLimitAppDemo/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace RateLimitAppDemo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [EnableRateLimiting("FixedPolicy")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries =
--- RateLimitAppDemo/Program.cs
+++ RateLimitAppDemo/Program.cs
@@ -1,12 +1,30 @@
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter(policyName: "FixedPolicy", options =>
+    {
+        options.PermitLimit = 5;
+        options.Window = TimeSpan.FromSeconds(10);
+        options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        options.QueueLimit = 2;
+    });
+    rateLimiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    rateLimiterOptions.OnRejected = async (context, token) =>
+    {
+        await context.HttpContext.Response.WriteAsync(
+            "Rate limit exceeded. Please try again later.", cancellationToken: token);
+    };
+});
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.UseRateLimiter();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,6 +36,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers().RequireRateLimiting("FixedPolicy");
 
 app.Run();
